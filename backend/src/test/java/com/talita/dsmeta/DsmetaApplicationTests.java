f30bd4c4079f772a22589d47c67c8527f3df5175
Tatafesan <talitafesan@gmail.com>
2022-07-17 10:14:01 -0400

    Project created

---
+++ backend/src/test/java/com/talita/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talita.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
