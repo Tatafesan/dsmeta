a6fbada704a25f6fa6bab187d1e8e0d852058ed3
Tatafesan <talitafesan@gmail.com>
2022-07-17 20:02:02 -0400

    API test

---
+++ backend/src/main/java/com/talita/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.talita.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.talita.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
